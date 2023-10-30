820fee1db35b6ac240d305b9f8f334876beded3d
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2023-10-30 15:27:07 +0000

    datatypes.js

---
+++ Basics/conversionops.js
@@ -0,0 +1,24 @@
+let score = "Ujjwal";
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "-1";
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ujjwal" => true
+
+let sumNumber = 33;
+
+let stringNumber = String(sumNumber);
+console.log(typeof stringNumber);
---
+++ Basics/datatypes.js
@@ -0,0 +1,23 @@
+//alert(3 + 3);
+
+console.log("Ujjwal");
+
+let name = "Ujjwal";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+
+// boolean => true/false
+
+// null (StandAlone value)
+// undefined => value not assigned
+// symbol => uniqueness
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
