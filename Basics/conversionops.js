ee0e950005b3b0897298931138c96507ce8c1e03
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2023-10-30 17:39:02 +0000

    comparison

--- Basics/conversionops.js
+++ Basics/conversionops.js
@@ -21,4 +21,30 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let sumNumber = 33;
 
 let stringNumber = String(sumNumber);
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ******* Operations *******
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = "ujjwal";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2; // 4
+
+let gameCounter = 100;
++gameCounter;
+console.log(gameCounter);
+
+
