33de13039420ee0d70dd096242f31b1e103d508b
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2023-11-01 20:52:36 +0000

    object1

--- Basics/objects.js
+++ Basics/objects.js
@@ -1,1 +1,39 @@
+// singleton or constructor method
+// Object.create()
+
+
 //object literals 
+
+const mySym = Symbol("key1");
+
+const jsUser = {
+    name: "Ujjwal",
+    "full name" : "uJJWAL sINHA",
+    [mySym] : "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "ujjwalsinha@gmail.com",
+    isLoggedIn: false,
+    lastLogindays: ["Monday", "Saturday"]
+};
+
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+jsUser.email = "ujjwal@chatgpt.com";
+// Object.freeze(jsUser);
+jsUser.email = "chatgpt@microsoft.com";
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello Js User");
+}
+
+console.log(jsUser.greeting);
+
+jsUser.greeting2 = function(){
+    console.log(`Hello Js User, ${this.name}`);
+}
+console.log(jsUser.greeting2());
