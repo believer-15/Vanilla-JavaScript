38a249eaf948c61eafab6b47e8b61ed75af5dda4
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2023-11-17 17:38:18 +0000

    object

--- objects.js
+++ objects.js
@@ -1,1 +1,20 @@
 //object literals 
+
+let val1 = 2;
+let val2 = 02;
+
+if(val1 == val2){
+    console.log("true");
+}else {
+    console.log("false");
+}
+
+const letters = new Set(["a","b","c"]);
+
+// List all Elements
+let text = "";
+letters.forEach (function(value) {
+  text += value;
+})
+
+console.log(letters.values());
