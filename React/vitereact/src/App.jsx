f9057078e26841708e76018e31a89430af98cfb4
believer-15 <ujjwalsinha15@gmail.com>
2023-12-01 03:54:03 +0530

    vitereact

---
+++ React/vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Coffee from "./Coffee"
+
+function App() {
+
+  return (
+    // <>, this is know as fragment in react we return only one element.So we wrap in fragment.
+    <> 
+      <Coffee></Coffee>
+      <h1>Ujjwal | Sinha</h1>
+    </>
+    
+  )
+}
+
+export default App
