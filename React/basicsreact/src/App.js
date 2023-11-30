64348f8c61b0b37c1816c5f451b547a530e98b76
believer-15 <ujjwalsinha15@gmail.com>
2023-12-01 03:52:56 +0530

    basicreact

---
+++ React/basicsreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <Chai></Chai>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ React/basicsreact/src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Have cup of tea</h1>
+    )
+}
+
+export default Chai
